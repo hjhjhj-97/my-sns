19716e946151f1088a8e4aef41497ea8c162474e
Hyunjung Hwang <ghkdguswjd115@gmail.com>
2025-09-26 11:43:21 +0900

    FeedDAO 개발

--- src/main/java/com/example/mysns/MainController.java
+++ src/main/java/com/example/mysns/MainController.java
@@ -1,10 +1,12 @@
 package com.example.mysns;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    public mainpage (){
+    @GetMapping("/")
+    public String mainPage (){
         return "feed.html";
     }
 }
